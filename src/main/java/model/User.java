021d807e8a128e0951f74374db61870b38feb7d4
Suraj Mishra <mishrasur7@gmail.com>
2022-09-30 00:05:46 +0300

    User clas made

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,16 +1,85 @@
 package model;
 
+import java.security.Timestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 @Entity
 public class User {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
 	private String email;
 	private String password;
+
+	@CreationTimestamp
+	private Timestamp createdTime;
+
+	@UpdateTimestamp
+	private Timestamp updatedTime;
+
+	public User() {
+	}
+
+	public User(String email, String password, Timestamp createdTime, Timestamp updatedTime) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.createdTime = createdTime;
+		this.updatedTime = updatedTime;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Timestamp getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(Timestamp createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	public Timestamp getUpdatedTime() {
+		return updatedTime;
+	}
+
+	public void setUpdatedTime(Timestamp updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + ", password=" + password + ", createdTime=" + createdTime
+				+ ", updatedTime=" + updatedTime + "]";
+	}
 
 }
