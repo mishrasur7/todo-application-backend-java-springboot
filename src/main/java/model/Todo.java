affd45ad786b8cc134e6dfd1fde9257fc4587e39
Suraj Mishra <mishrasur7@gmail.com>
2022-10-03 10:04:38 +0300

    todo id added

--- src/main/java/model/Todo.java
+++ src/main/java/model/Todo.java
@@ -1,5 +1,18 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Todo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id; 
+	
+	
 
 }
